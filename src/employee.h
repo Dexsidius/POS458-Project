b387bd04486bbb4b7fe3638e9d831ee01515240b
Da'Shawn Larry <dashawnlarry@gmail.com>
2020-11-29 10:55:22 -0500

    Update employee.h
    added empID, constructor for server and manager

--- src/employee.h
+++ src/employee.h
@@ -7,25 +7,31 @@
 
 class Server{
     public:
+    Server();
+    Server(string, char, char, float);
+
     string name;
 
-    //float tips, hourly rate, tickets
-
-    void Clock_in();
-
-    private:
     char password;
+    char empID;
     float hourly_rate;
     float tips;
+
+    void Clock_in();
+    
 
 };
 
 class Manager: public Server{
     public:
 
+    Manager(string, char, char, float);
+
     private:
 
     void addUser();
+    void viewTickets();
+    void viewEmpolyeeInfo(char);
 };
 
 #endif employee_h
